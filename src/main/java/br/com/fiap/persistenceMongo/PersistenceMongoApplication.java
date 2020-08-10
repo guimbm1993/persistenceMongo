76c64b38de0c240366915a22c5828a571ad92530
Guilherme Moraes <guilhermemoraes@MacBook-Air-de-Guilherme.local>
2020-08-10 20:59:58 -0300

    commit inicial

---
+++ src/main/java/br/com/fiap/persistenceMongo/PersistenceMongoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.persistenceMongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistenceMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersistenceMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/persistenceMongo/PersistenceMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.persistenceMongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistenceMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
